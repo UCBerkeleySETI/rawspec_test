3090eda423a6ba112ca78c9449d2fb1a9898c629
Richard Elkins <richard.elkins@gmail.com>
2021-11-13 16:45:02 -0600

    Update installer.py

--- exec/installer.py
+++ exec/installer.py
@@ -10,10 +10,10 @@ Creates/recreates the rawspec testing baseline.
 * For each .raw file in the baseline directory, do the following:
     - rawspec   -f 1048576   -t 51   <.raw file prefix>
 * For each .fil file in the baseline directory produced by rawspec, do the following:
-    - peek <0000.fil file> > <peeked file>
     - turboSETI   -n 64   -s 10   -g y   -d <GPU_ID>   <0000.fil file>
-    - python3   dat2tbl.py     <.dat file>   <.tbldat file>
-    - python3   installer.py   <.h5 file>    <.tblhdr file>
+    - Create a .tbldat file.
+    - Create a .tblhdr file.
+    - Create a .tbldsel file.
 * Cleanup: rm  *.fil  *.h5  *.dat  *.log in the baseline directory.
 """
 
@@ -152,10 +152,6 @@ def main(args=None):
 
     # For each unique 0000.fil, run turbo_seti, dat2tbl, and hdr2tbl.
     for filfile in sorted(glob.glob("*.fil")):
-        peekfile = filfile.split("/")[-1].replace(".fil", ".peeked")
-        cmd = "peek  {} 2>&1 > {}" \
-              .format(filfile, peekfile)
-        run_cmd(cmd, logger)
         cmd = "turboSETI  --snr {}  --gpu y  --gpu_id {}  --n_coarse_chan 64  {}" \
               .format(TS_SNR_THRESHOLD, args.gpu_id, filfile)
         run_cmd(cmd, logger)
