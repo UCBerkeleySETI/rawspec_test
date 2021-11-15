7c232f40483676b28b169160a5aa6c4d820f7b63
Richard Elkins <richard.elkins@gmail.com>
2021-11-15 09:11:22 -0600

    Update common.py

--- exec/common.py
+++ exec/common.py
@@ -22,7 +22,7 @@ RAWSPEC_TESTING_DIR = "/mnt_blpd20/scratch/rawspec_testing/"
 
 ATA_DIR = RAWSPEC_TESTING_DIR + "ata/" # Only used in the UC Berkeley data centre as a source of files to link.
 
-SELECTED = [
+SELECTED = [ # List of files to actually link into baseline and trial directories
     SOURCE_DIR_1 + "blc13_guppi_57991_49836_DIAG_FRB121102_0010",
     SOURCE_DIR_2 + "blc17_guppi_57991_49318_DIAG_PSR_J0332+5434_0008",
     ]
