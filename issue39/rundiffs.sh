c686fef16949577bf80bb20f191fef092db404d9
Richard Elkins <richard.elkins@gmail.com>
2021-11-24 13:26:02 -0600

    Update rundiffs.sh

--- issue39/rundiffs.sh
+++ issue39/rundiffs.sh
@@ -1,11 +1,31 @@
-source rungpu.cfg
+GPUA=0
 
-echo 
-echo diff for ics
+function oops {
+	echo
+	echo '*** Oops *** '$1' !! ***' 2>&1 | tee -a $LOG
+	echo
+	exit 86
+}
+
+function check_gpu_id {
+    case "$1" in
+        (*[!0-9]*)
+            oops 'The specified GPU ID is not numeric: '$1
+    esac
+    if [ "$1" -gt 3 ]; then
+        MSG='The specified GPU ID must be 0, 1, 2, or 3 but I saw '$1
+        echo $MSG  2>&1 | tee -a $LOG
+    fi
+ }
+
+# One command line argument: GPU ID.
+
+NARGS=$#
+[ $NARGS -ne 1 ] && oops '1 argument must be supplied: GPU ID (0, 1, 2, or 3)'
+GPUB=$1
+check_gpu_id $GPUB
+
+set -x
 diff gpu$GPUA.ics.tbldsel gpu$GPUB.ics.tbldsel
-echo 
-echo diff for ant001
 diff gpu$GPUA.ant001.tbldsel gpu$GPUB.ant001.tbldsel
-echo 
-echo diff for ant000
 diff gpu$GPUA.ant000.tbldsel gpu$GPUB.ant000.tbldsel
