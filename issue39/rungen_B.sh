d74ec90f25cbff66f32d527d8096138c32085920
Richard Elkins <richard.elkins@gmail.com>
2021-11-25 11:31:30 -0600

    Create rungen_B.sh

---
+++ issue39/rungen_B.sh
@@ -0,0 +1,42 @@
+set -e
+set -o pipefail
+
+export PATH=$HOME/rawspec:$PATH
+export LD_LIBRARY_PATH=$HOME/rawspec
+STEM=/mnt_blpd20/scratch/rawspec_testing/baseline//ATA_guppi_59229_47368_006379_40blocks
+PARMS1='-f 8192 -t 2 -S -d . '
+PARMS2='-f 8192 -t 2 -i '1.0'  -d . '
+
+function oops {
+    echo
+    echo '*** Oops *** '$1' !! ***' 2>&1 | tee -a $LOG
+    echo
+    exit 86
+}
+
+function testsession {
+    LOG=gpu$1.log
+    echo 2>&1 | tee $LOG
+    echo Start GPU $1 2>&1 | tee -a $LOG
+    (set -x; rawspec -g $1 $PARMS1 $STEM 2>&1 | tee -a $LOG)
+    if [ $? -ne 0 ]; then
+        oops rawspec 1 exit status $?
+    fi
+    (set -x; rawspec -g $1 $PARMS2 $STEM 2>&1 | tee -a $LOG)
+    if [ $? -ne 0 ]; then
+        oops rawspec 2 exit status $?
+    fi
+    python3 dsel2tbl.py $STEM-ant000.rawspec.0000.fil gpu$1.ant000.tbldsel 2>&1 | tee -a $LOG
+    if [ $? -ne 0 ]; then
+        oops dsel2tbl.py exit status $?
+    fi
+    python3 dsel2tbl.py $STEM-ant001.rawspec.0000.fil gpu$1.ant001.tbldsel 2>&1 | tee -a $LOG
+    python3 dsel2tbl.py $STEM-ics.rawspec.0000.fil gpu$1.ics.tbldsel 2>&1 | tee -a $LOG
+}
+
+rm gpu*.*
+
+testsession 0
+testsession 1
+testsession 2
+testsession 3
